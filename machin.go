f8de51607f4bdc8ef4af376ab78517107945ba4f
patrick nsukami <ndkpatt@gmail.com>
2015-07-13 05:54:27 +0000

    Basic config for sql table creation.

--- machin.go
+++ machin.go
@@ -3,11 +3,41 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+	"log" // logging operations
+	"os" // os operations
 )
+
+func boing(){
+	fmt.Println("boing")
+	// remove db file if exists
+	os.Remove("./test.db")
+
+	// get connection to db
+	db, err := sql.Open("sqlite3", "./test.db")
+	// log error if any
+	if err != nil {
+		log.Fatal(err)
+	}
+	// ensure db is closed
+	defer db.Close()
+
+	sqlStmt := `
+	create table if not exists person (id integer not null primary key, name text);
+	`
+	// log error if any
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		log.Printf("%q: %s\n", err, sqlStmt)
+		return
+	}
+}
 
 func homepage(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "Helo")
+	boing()
 }
 
 func main() {
