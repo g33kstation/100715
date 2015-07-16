01a322922103add317cc4ff2457130239daa8781
patrick nsukami <ndkpatt@gmail.com>
2015-07-16 21:50:39 +0000

    Basic structure for unit testing main package.

---
+++ main_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	// "fmt"
+	//"net/http"
+	// "database/sql"
+	// _ "github.com/mattn/go-sqlite3"
+	// "log"
+	// "os"
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHomePage(t *testing.T) {
+	assert.True(t, true, "This is good. Canary test passing")
+}
